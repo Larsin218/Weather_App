a30f5797815434cfb25b50313fe8767ac4c595b3
Luke Smith <larsin.218@gmail.com>
2024-05-09 03:50:43 -0600

    api stuff

--- dev/script.js
+++ dev/script.js
@@ -1,3 +1,13 @@
-// api key: 
 
-// 
+
+function getApi() {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function () {
+        
+    })
+}
